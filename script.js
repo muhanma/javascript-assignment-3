f2babd908389e26f258224fed28beb64aec53fd4
muhanma <muhan.ma@id.iit.edu>
2017-04-20 22:03:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 // Do your work in this file.
+//--Do your work in this file.--//
+//--1. Fork this repo.--//
+//--2. Only code inside the file named script.js. No editing any other files--//
 
+//--3. Create a div element with a class name of "view" and append it to the document body.
+
+var view = document.createElement("div");
+view.className = "view";
+document.body.appendChild(view);
+
+//--4. Create a div element with a class name of "grass" and append it to the previously created "view" div from step 3.
+var grass = document.createElement("div");
+grass.className = "grass";
+view.appendChild(grass);
+
+//--5. Create a div element with a class name of "sun" and append it to the previously created "view" div from step 3.
+var sun = document.createElement("div");
+sun.className = "sun";
+view.appendChild(sun);
+
+//--6. The div classes will be "ray-1", "ray-2", "ray-3", "ray-4", and "ray-5". 
+var rayN = 1;
+while (rayN <= 5) {
+    var ray = document.createElement("div");
+    ray.className = "ray-"+rayN;
+    sun.appendChild(ray);
+    rayN++;
+}
+//--7.Create a div element with a class name of "mountain" and append it to the previously created "view" div from step 3.
+var mountain = document.createElement("div");
+mountain.className = "mountain";
+view.appendChild(mountain);
+//--8.Create a div element with a class name of "top" and append it to the previously created "mountain" div from 
+var top = document.createElement("div");
+top.className = "top";
+mountainDiv.appendChild(top);
+
+//--9.do while, mountain cap
+var cap = 1;
+do {
+    var cap = document.createElement("div");
+    cap.className = "mountain-cap-"+ cap;
+    top.appendChild(cap);
+    cap++;
+} while (cap < 4);
